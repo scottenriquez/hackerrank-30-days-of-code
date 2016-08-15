c008801013e000ccc1e3a261144a61f2e31608a4
Scott Enriquez <scottie@enriquez.io>
2016-08-15 17:41:00 -0500

    Adding the code for the second day

---
+++ day2/ruby/day2.rb
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+integer = 4
+double = 4.0
+string = 'HackerRank'
+
+# Read and save an integer, double, and String to your variables.
+integer = gets
+double = gets
+string = gets
+
+# Print the sum of both integer variables on a new line.
+puts i + integer.to_i
+
+# Print the sum of the double variables on a new line.
+puts d + double.to_f
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + string.to_s
